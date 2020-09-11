32a30ed8cb0675aa29d9e955dfeff27c8c16750e
evndroo <evandro.slipjr@gmail.com>
2020-09-11 15:53:17 -0300

    adicionando botão go to top

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-let btn = document.querySelector("#toggle-menu");
-let nav = document.querySelector("nav");
+const btn = document.querySelector("#toggle-menu");
+const nav = document.querySelector("nav");
+const toTop = document.querySelector("#top-button");
+
 
 btn.onclick = () =>{
     nav.classList.toggle("showing-nav")
 }
+
+
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > (window.visualViewport.height / 2 )) {
+    toTop.classList.add("visible");
+  } else {
+    toTop.classList.remove("visible");
+  }
+})
+
